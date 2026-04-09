f2774f244d458372a9d6401224de725decf530d0
Kassandra Negrete <kassandra.negretec@gmail.com>
2026-04-08 18:04:05 -0600

    Limpiar staging y ejecutar el servidor basico usando node.js con express

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
 
+// Iniciar el servidor
 app.listen(3000, () => {
   console.log("Servidor en puerto 3000");
 });
